44dfd31e4e09134e3ce2f2cd78f6f89eaa035607
Efrain Diaz <efradiaz28@gmail.com>
2023-02-08 23:23:03 -0500

    them.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String saludar;
+        saludar = "Hola mundo!";
+        System.out.println(saludar);
+    }
+
+}
